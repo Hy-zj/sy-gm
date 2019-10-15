fe2880717ec4122c8135c1928a63f7942df4137c
huangyue <2552686730@qq.com>
2019-10-15 11:20:58 +0800

    顶部广告弹窗

---
+++ src/components/Advertisement.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="gg-header">
+    <div class="one"></div>
+    <div class="two"></div>
+    <div class="stree"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Advertisement'
+}
+</script>
+
+<style scoped>
+.gg-header{
+  width: 375px;
+  height: 50px;
+  background: red;
+}
+</style>
