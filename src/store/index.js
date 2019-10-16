e5ad1de910094ceffb684719fbcedd289cd0a223
jm <476357219@qq.com>
2019-10-16 17:32:32 +0800

    分类页面提交

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
 Vue.use(Vuex)
-
 export default new Vuex.Store({
   state: {
-
+    itemObj: {}
   },
   mutations: {
-
+insert(state){
+  Vue.axios.get('/data/list-tab-title.json').then((response)=>{
+    console.log(response)
+    state.itemObj=response;
+  })
+}
   },
   actions: {
-
+    insert (context) {
+      context.commit('insert')
+    }
   }
 })
--- src/views/list.vue
+++ src/views/list.vue
@@ -1,51 +1,42 @@
 <template>
   <div>
-    <div class="header">
-     <van-icon name="arrow-left" class="arrow-left"/>
-      <input type="text" placeholder="家居家装 好物低至5折">
-      <van-icon name="ellipsis" class="ellipsis"/>
-    </div>
+    <topSearch></topSearch>
+    <van-tree-select height="55vw" :items="items" :main-active-index.sync="activeIndex">
+      <template slot="content">
+        <!-- <van-image v-if="activeIndex === 0" src="https://img.yzcdn.cn/vant/apple-1.jpg" />
+        <van-image v-if="activeIndex === 1" src="https://img.yzcdn.cn/vant/apple-2.jpg" />-->
+      </template>
+    </van-tree-select>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
-import { Icon } from "vant";
 
+import { TreeSelect } from "vant";
+Vue.use(TreeSelect);
+import topSearch from "../components/list/topSearch";
 export default {
-  name: 'list'
-}
-</script>
-
-Vue.use(Icon);
-export default {
+  name: "list",
+  components: {
+    topSearch
+  },
+  data() {
+    return {
+      activeIndex: 0,
+      items: [{ text: "分组 1" }, { text: "分组 2" }]
+    };
+  },
+  mounted () {
+    this.getData()
+  },
   methods: {
-   
+    getData() {
+      this.$store.commit('insert')
+    }
   }
 };
 </script>
 
 <style scoped>
-.header {
-  width: 100%;
-  height: 44px;
-  display: flex;
-  justify-content: space-around;
-}
-.header .arrow-left {
-  font-size: 22px;
-  color: #646464;
-}
-input{
-    width: 268px;
-    height: 30px;
-    background-color: #f8f8f8;
-    outline: none;
-    border: none;
-    border-radius: 15px;
-}
-.ellipsis{
-    font-size: 22px;
-  color: #646464;
-}
 </style>
